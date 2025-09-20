401fa3e8b61974b2cf3a07df86962cfeb97d2a20
Nicolaj-Rux <rux@math.tu-berlin.de>
2025-09-20 22:10:12 +0200

    Cleaned old files

--- dev/utils.py
+++
@@ -1,9 +0,0 @@
-def find_sup_key(dictionary, key):
-    """Returns key_sup that satisfies key == key_sup in d, T, L
-        and key <= key_sup in K, J, else None"""
-    d0, T0, L0, K0, J0 = key
-    for key_sup in dictionary:
-        d, T, L, K, J = key_sup
-        if d0 == d and T0 == T and L0 == L and K0 <= K and J0 <= J:
-            return key_sup
-    return None
